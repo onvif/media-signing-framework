78fdfcc1efb45b9cc65fe860bc34c869f60cdd74
bjornvolcker <bjornvolcker@users.noreply.github.com>
2024-05-11 08:33:09 +0200

    Adds example code for signing and validation
    An examples folder with a signer and a validator application
    is added. These are based on gStreamer and requires
    additional dependencies to build.
    
    The example applications are not complete, but serves so far
    as example code.
    
    A shell script to build the applications is added.
    Further, to be able to build the library and the applications,
    meson files have been added and also a dummy definitions file,
    which will be replaced when the proper code has been added.
    
    Also, workflow actions are added for testing in the repository.

---
+++ lib/src/oms_tmp_definitions.c
@@ -0,0 +1,218 @@
+/************************************************************************************
+ * Copyright (c) 2024 ONVIF.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *    * Neither the name of ONVIF nor the names of its contributors may be
+ *      used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL ONVIF BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ************************************************************************************/
+
+#include <stdlib.h>  // free
+#include <string.h>  // strcmp
+
+#include "includes/onvif_media_signing_common.h"
+#include "includes/onvif_media_signing_signer.h"
+#include "includes/onvif_media_signing_validator.h"
+
+/* Public onvif_media_signing_common.h APIs */
+onvif_media_signing_t *
+onvif_media_signing_create(MediaSigningCodec codec)
+{
+  if ((codec < 0) || (codec >= OMS_CODEC_NUM)) return NULL;
+
+  return NULL;
+}
+
+MediaSigningReturnCode
+onvif_media_signing_reset(onvif_media_signing_t *self)
+{
+  if (!self) return OMS_INVALID_PARAMETER;
+  return OMS_OK;
+}
+
+void
+onvif_media_signing_free(onvif_media_signing_t *self)
+{
+  free(self);
+}
+
+const char *
+onvif_media_signing_get_version()
+{
+  return "0.0.0";
+}
+
+int
+onvif_media_signing_compare_versions(const char *version1, const char *version2)
+{
+  return strcmp(version1, version2);
+}
+
+/* onvif_media_signing_signer APIs */
+MediaSigningReturnCode
+onvif_media_signing_add_nalu_for_signing(onvif_media_signing_t *self,
+    const uint8_t *nalu,
+    size_t nalu_size,
+    int64_t timestamp)
+{
+  return (!self || !nalu || nalu_size == 0 || timestamp == 0) ? OMS_INVALID_PARAMETER
+                                                              : OMS_OK;
+}
+
+MediaSigningReturnCode
+onvif_media_signing_add_nalu_part_for_signing(onvif_media_signing_t *self,
+    const uint8_t *nalu_part,
+    size_t nalu_part_size,
+    int64_t timestamp,
+    bool is_last_part)
+{
+  return (!self || !nalu_part || nalu_part_size == 0 || timestamp == 0)
+      ? OMS_INVALID_PARAMETER
+      : (is_last_part ? OMS_OK : OMS_NOT_SUPPORTED);
+}
+
+MediaSigningReturnCode
+onvif_media_signing_get_sei(onvif_media_signing_t *self, oms_sei_to_add_t *sei)
+{
+  return (!self || !sei) ? OMS_INVALID_PARAMETER : OMS_OK;
+}
+
+MediaSigningReturnCode
+onvif_media_signing_set_signing_key_pair(onvif_media_signing_t *self,
+    const char *private_key,
+    size_t private_key_size,
+    const char *certificate_chain,
+    size_t certificate_chain_size,
+    bool user_provisioned)
+{
+  return (!self || !private_key || private_key_size == 0 || !certificate_chain ||
+             certificate_chain_size == 0)
+      ? OMS_INVALID_PARAMETER
+      : (!user_provisioned ? OMS_OK : OMS_NOT_SUPPORTED);
+}
+
+MediaSigningReturnCode
+onvif_media_signing_set_hash_algo(onvif_media_signing_t *self, const char *hash_algo_oid)
+{
+  return (!self || !hash_algo_oid) ? OMS_INVALID_PARAMETER : OMS_OK;
+}
+
+MediaSigningReturnCode
+onvif_media_signing_set_product_info(onvif_media_signing_t *self,
+    const onvif_media_signing_product_info_t *product_info)
+{
+  return (!self || !product_info) ? OMS_INVALID_PARAMETER : OMS_OK;
+}
+
+MediaSigningReturnCode
+onvif_media_signing_set_sei_epb(onvif_media_signing_t *self, bool sei_epb)
+{
+  return (!self) ? OMS_INVALID_PARAMETER : (sei_epb ? OMS_OK : OMS_NOT_SUPPORTED);
+}
+
+MediaSigningReturnCode
+onvif_media_signing_set_signing_frequency(onvif_media_signing_t *self,
+    unsigned signing_frequency)
+{
+  return (!self || signing_frequency == 0) ? OMS_INVALID_PARAMETER : OMS_OK;
+}
+
+MediaSigningReturnCode
+onvif_media_signing_set_max_signing_nalus(onvif_media_signing_t *self,
+    unsigned max_signing_nalus)
+{
+  return (!self || max_signing_nalus == 1) ? OMS_INVALID_PARAMETER : OMS_OK;
+}
+
+MediaSigningReturnCode
+onvif_media_signing_set_use_start_of_stream_sei(onvif_media_signing_t *self,
+    bool use_start_of_stream_sei)
+{
+  return (!self) ? OMS_INVALID_PARAMETER
+                 : (!use_start_of_stream_sei ? OMS_OK : OMS_NOT_SUPPORTED);
+}
+
+MediaSigningReturnCode
+onvif_media_signing_set_low_bitrate_mode(onvif_media_signing_t *self, bool low_bitrate)
+{
+  return (!self) ? OMS_INVALID_PARAMETER : (low_bitrate ? OMS_OK : OMS_NOT_SUPPORTED);
+}
+
+MediaSigningReturnCode
+onvif_media_signing_set_max_sei_payload_size(onvif_media_signing_t *self,
+    size_t max_sei_payload_size)
+{
+  return (!self || max_sei_payload_size == 1) ? OMS_INVALID_PARAMETER : OMS_OK;
+}
+
+MediaSigningReturnCode
+onvif_media_signing_get_start_of_stream_sei(onvif_media_signing_t *self,
+    uint8_t *sei,
+    size_t *sei_size)
+{
+  return (!self || !sei || sei_size == 0) ? OMS_INVALID_PARAMETER : OMS_OK;
+}
+
+MediaSigningReturnCode
+onvif_media_signing_set_end_of_stream(onvif_media_signing_t *self)
+{
+  return !self ? OMS_INVALID_PARAMETER : OMS_OK;
+}
+
+/* onvif_media_signing_validator APIs */
+void
+onvif_media_signing_authenticity_report_free(
+    onvif_media_signing_authenticity_t *authenticity_report)
+{
+  free(authenticity_report);
+}
+
+onvif_media_signing_authenticity_t *
+onvif_media_signing_get_authenticity_report(onvif_media_signing_t *self)
+{
+  return !self ? NULL : NULL;
+}
+
+MediaSigningReturnCode
+onvif_media_signing_add_nalu_and_authenticate(onvif_media_signing_t *self,
+    const uint8_t *nalu,
+    size_t nalu_size,
+    onvif_media_signing_authenticity_t **authenticity)
+{
+  return (!self || !nalu || nalu_size == 0 || authenticity) ? OMS_INVALID_PARAMETER
+                                                            : OMS_OK;
+}
+
+MediaSigningReturnCode
+onvif_media_signing_set_root_certificate(onvif_media_signing_t *self,
+    const char *root_cert,
+    size_t root_cert_size)
+{
+  return (!self || !root_cert || root_cert_size == 0) ? OMS_INVALID_PARAMETER : OMS_OK;
+}
+
+bool
+onvif_media_signing_is_start_of_stream_sei(onvif_media_signing_t *self,
+    const uint8_t *nalu,
+    size_t nalu_size)
+{
+  return (!self || !nalu || nalu_size == 0) ? false : true;
+}
---
+++ test_apps.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Set GST_PLUGIN_PATH so gStreamer can find local gst-elements
+export GST_PLUGIN_PATH=$PWD/local_installs
+VALIDATOR_PATH=$PWD/validator_installs
+VALIDATOR=$VALIDATOR_PATH/bin/validator
+SIGNER=$GST_PLUGIN_PATH/bin/signer
+# Remove all old stuff
+rm -rf build_lib_for_signing
+rm -rf build_lib_for_validation
+rm -rf build_signer
+rm -rf build_validator
+rm -rf $GST_PLUGIN_PATH
+rm -rf $VALIDATOR_PATH
+rm validation_results.txt
+rm signed_test_h264.mp4
+rm signed_test_h265.mp4
+rm test_h264.mp4
+rm test_h265.mp4
+rm private_ecdsa_key.pem
+rm public_ecdsa_key.pem
+
+# Build and install library for apps
+meson -Dbuildtype=debug -Ddebugprints=false --prefix $GST_PLUGIN_PATH . build_lib_for_signing
+meson install -C build_lib_for_signing
+
+meson -Dbuildtype=debug -Ddebugprints=false --prefix $VALIDATOR_PATH . build_lib_for_validation
+meson install -C build_lib_for_validation
+
+# Build and install apps
+meson -Dsigner=true -Dbuildtype=debug --prefix $GST_PLUGIN_PATH examples build_signer
+meson install -C build_signer
+
+meson -Dvalidator=true -Dbuildtype=debug --prefix $VALIDATOR_PATH examples build_validator
+meson install -C build_validator
+
+# Copy file to current directory
+cp examples/test-files/test_h264.mp4 .
+$SIGNER test_h264.mp4
+$VALIDATOR -c h264 signed_test_h264.mp4
+
+cat validation_results.txt
