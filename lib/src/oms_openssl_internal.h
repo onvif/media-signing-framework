52035208eaaede13592b154dfad41ca0c3b80f68
Björn Völcker <65532189+bjornvolcker@users.noreply.github.com>
2024-05-16 12:27:11 +0200

    Moves key generators to public helper file (#6)
    Generating signing key pairs are helpful during development
    and for testing on device. Therefore, make the APIs public
    to be reachable from outside the library.
    
    Co-authored-by: bjornvolcker <bjornvolcker@users.noreply.github.com>

--- lib/src/oms_openssl_internal.h
+++ lib/src/oms_openssl_internal.h
@@ -32,6 +32,8 @@
 #include <stdint.h>  // uint8_t
 #include <string.h>  // size_t
 
+#include "includes/onvif_media_signing_common.h"
+
 /**
  * Struct for storing necessary information to sign a hash and generate a signature.
  * It is used primarily by the signing plugins.
@@ -113,46 +115,6 @@ void
 openssl_free_key(void *key)
 {
   assert(key);
-}
-
-/**
- * @brief Helper functions to generate a private key
- *
- * Two different APIs for RSA and ECDSA. By specifying a location a PEM file is generated
- * and stored as private_rsa_key.pem or private_ecdsa_key.pem. The user can then read this
- * file and pass the content to Signed Video through signed_video_set_private_key_new().
- * In addition to storing as file the content can be written to buffers at once. Memory is
- * allocated for |private_key| and the content of |private_key_size| Bytes is written.
- * Note that the ownership is transferred.
- *
- * Writing to file currently only works on Linux.
- *
- * @param dir_to_key If not NULL, the location where the PEM file will be written.
- *   Null-terminated string.
- * @param private_key If not NULL the content of the private key PEM file is copied to
- *   this output. Ownership is transferred.
- * @param private_key_size If not NULL outputs the size of the |private_key|.
- *
- * @returns OMS_OK Valid algorithm and successfully written PEM-file,
- *          OMS_NOT_SUPPORTED Algorithm is not supported,
- *          OMS_INVALID_PARAMETER Invalid input parameter,
- *          OMS_EXTERNAL_ERROR PEM-file could not be written.
- */
-MediaSigningReturnCode
-oms_generate_ecdsa_private_key(const char *dir_to_key,
-    char **private_key,
-    size_t *private_key_size)
-{
-  return (dir_to_key || (private_key && private_key_size)) ? OMS_OK
-                                                           : OMS_INVALID_PARAMETER;
-}
-MediaSigningReturnCode
-oms_generate_rsa_private_key(const char *dir_to_key,
-    char **private_key,
-    size_t *private_key_size)
-{
-  return (dir_to_key || (private_key && private_key_size)) ? OMS_OK
-                                                           : OMS_INVALID_PARAMETER;
 }
 
 #endif  // __OMS_OPENSSL_INTERNAL_H__
