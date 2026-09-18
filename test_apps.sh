0d0cf3ed8272311ff6160af91da071adb1ec2174
Sriram Bhetanabottla <sriram.bhetanabottla@axis.com>
2026-09-18 09:58:41 +0200

    fix: integrate tamperer into app builds

--- test_apps.sh
+++ test_apps.sh
@@ -5,7 +5,20 @@ export GST_PLUGIN_PATH=$PWD/local_installs
 VALIDATOR_PATH=$PWD/validator_installs
 VALIDATOR=$VALIDATOR_PATH/bin/validator
 SIGNER=$GST_PLUGIN_PATH/bin/signer
-TAMPERER=$PWD/examples/tools/tamperByte
+TAMPERER=$VALIDATOR_PATH/bin/tamperByte
+RESULTS_PATH=$PWD/app_test_results
+
+validate()
+{
+  result_name=$1
+  shift
+  echo ""
+  echo "=== Validate $result_name ==="
+  "$VALIDATOR" "$@"
+  cp validation_results.txt "$RESULTS_PATH/$result_name.txt"
+  cat "$RESULTS_PATH/$result_name.txt"
+}
+
 if [ $# -eq 0 ]
   then
     echo "No arguments supplied"
@@ -18,7 +31,8 @@ rm -rf build_signer
 rm -rf build_validator
 rm -rf $GST_PLUGIN_PATH
 rm -rf $VALIDATOR_PATH
-# rm validation_results.txt
+rm -rf "$RESULTS_PATH"
+mkdir -p "$RESULTS_PATH"
 
 echo ""
 echo "=== Build the signer example app ==="
@@ -36,28 +50,22 @@ meson setup -Dvalidator=true -Dbuildtype=debug -D$PARSESEI --prefix $VALIDATOR_P
 meson install -C build_validator
 
 # Sign and validate test files
-$VALIDATOR -c h264 examples/test-files/test_h264.mp4
-cat validation_results.txt
+validate unsigned_h264 -c h264 examples/test-files/test_h264.mp4
 
 $SIGNER -c h264 examples/test-files/test_h264.mp4
-$VALIDATOR -C test -c h264 examples/test-files/signed_test_h264.mp4
-cat validation_results.txt
+validate signed_h264 -C test -c h264 examples/test-files/signed_test_h264.mp4
 
 $TAMPERER examples/test-files/signed_test_h264.mp4 examples/test-files/tampered_signed_test_h264.mp4
-$VALIDATOR -C test -c h264 examples/test-files/tampered_signed_test_h264.mp4
-cat validation_results.txt
+validate tampered_signed_h264 -C test -c h264 examples/test-files/tampered_signed_test_h264.mp4
 rm examples/test-files/signed_test_h264.mp4
 rm examples/test-files/tampered_signed_test_h264.mp4
 
-$VALIDATOR -b -c h265 examples/test-files/test_h265.mp4
-cat validation_results.txt
+validate unsigned_h265 -b -c h265 examples/test-files/test_h265.mp4
 
 $SIGNER -c h265 examples/test-files/test_h265.mp4
-$VALIDATOR -b -C test -c h265 examples/test-files/signed_test_h265.mp4
-cat validation_results.txt
+validate signed_h265 -b -C test -c h265 examples/test-files/signed_test_h265.mp4
 
 $TAMPERER examples/test-files/signed_test_h265.mp4 examples/test-files/tampered_signed_test_h265.mp4
-$VALIDATOR -C test -c h265 examples/test-files/tampered_signed_test_h265.mp4
-cat validation_results.txt
+validate tampered_signed_h265 -C test -c h265 examples/test-files/tampered_signed_test_h265.mp4
 rm examples/test-files/signed_test_h265.mp4
 rm examples/test-files/tampered_signed_test_h265.mp4
